5fe770295a7daa571f94a81d523293f42f22a8e6
Julian Adler <julian.dot.adler@gmail.com>
2022-06-19 10:22:42 +1200

    I can't believe it took so long to fix this.

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 const MainPage = () => {
+  const navigate = useNavigate();
+
   return (
     <React.Fragment>
       Menu shared by all the pages
       <br />
       <Link to="content1">content1</Link>
       <br />
-      <Link to="content2">content2</Link>
+      <span
+        onClick={() => {
+          const d = new Date();
+          let seconds = d.getSeconds();
+          if (seconds > 30) {
+            console.log("You can go", seconds);
+            navigate("content2");
+          } else {
+            console.log("You cannot go", seconds);
+          }
+        }}
+      >
+        content2
+      </span>
       <br />
       <br />
       <Link to="settings">settings</Link>
